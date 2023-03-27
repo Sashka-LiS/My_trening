471f350f3df5872bfc6d8c6b006f9a3e72d91f2c
Alexandr Lisimenko <lisimenko.alexander@yandex.ru>
2023-03-27 11:46:11 +0300

    update class_traning.py

--- class_trening.py
+++ class_trening.py
@@ -19,15 +19,16 @@ def del_contact():
     name = input("Name >>> ")
     del contacts[name]
 
-def print_menu(menu):
-    print(menu[0])
-    for i in menu[1:]:
-        print(i.title)
+def print_menu(name_menu, menu):
+    num_item = 0
+    print(name_menu)
+    for i in menu:
+        num_item += 1
+        print(f"{num_item} - {i.title}")
 
 
-main_menu = ["_____MENU_____",
-             Menuitem("Adding a new contact...", add_contact_mode),
+main_menu = [Menuitem("Adding a new contact...", add_contact_mode),
              Menuitem("Display list of contacts...", show_book),
              Menuitem("Delete selected contact...", del_contact)]           
 
-print_menu(main_menu)
+print_menu("_____MENU_____", main_menu)
