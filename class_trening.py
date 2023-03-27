4b7be19e007780d455d8c01edfe9dd94b4e0ae68
Alexandr Lisimenko <lisimenko.alexander@yandex.ru>
2023-03-27 12:24:59 +0300

    update class_traning.py

--- class_trening.py
+++ class_trening.py
@@ -25,10 +25,20 @@ def print_menu(name_menu, menu):
     for i in menu:
         num_item += 1
         print(f"{num_item} - {i.title}")
+    response = (input("Select a menu item --> "))
+    while not is_valid_response(response, menu):
+        response = (input("Select a menu item --> "))
+    return response
 
+    
+def is_valid_response(response, menu):
+    if not response.isdigit() or int(response) < 0 or int(response) > len(menu):
+        return False
+    return True
 
 main_menu = [Menuitem("Adding a new contact...", add_contact_mode),
              Menuitem("Display list of contacts...", show_book),
              Menuitem("Delete selected contact...", del_contact)]           
 
 print_menu("_____MENU_____", main_menu)
+# print(len(main_menu))
